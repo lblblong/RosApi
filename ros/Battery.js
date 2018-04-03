71d173493e1528fa1480a1253f8458e700790df5
lblblong <lblblong@qq.com>
2018-04-03 17:33:59 +0800

    修改返回为原始数据

--- ros/Battery.js
+++ ros/Battery.js
@@ -1,34 +1,34 @@
 let ROSLIB = require('roslib')
 
 class Battery {
-  constructor() {
-    this.data = ''
+    constructor() {
+        this.data = ''
 
-    global.event.on(global.events.ROS_CONNECTED, () => {
-      this.initDate()
-    })
+        global.event.on(global.events.ROS_CONNECTED, () => {
+            this.initDate()
+        })
 
-    global.event.on(global.events.ROS_DISCONNECTED, () => {
-      this.data = ''
-    })
-  }
+        global.event.on(global.events.ROS_DISCONNECTED, () => {
+            this.data = ''
+        })
+    }
 
-  initDate() {
-    let topic = new ROSLIB.Topic({
-      ros: global.ros,
-      name: '/battery'
-    })
+    initDate() {
+        let topic = new ROSLIB.Topic({
+            ros: global.ros,
+            name: '/battery'
+        })
 
-    topic.subscribe(msg => {
-      let rep = msg.data
-      this.data = {
-        status: rep[0],
-        // 取 mAh 电量需要除以 360
-        capacity: rep[1] / 360,
-        soc: rep[2] / 360
-      }
-    })
-  }
+        topic.subscribe(msg => {
+            let rep = msg.data
+            // 如果取毫安时电量，需要除以360
+            this.data = {
+                status: rep[0],
+                capacity: rep[1],
+                soc: rep[2]
+            }
+        })
+    }
 }
 
 module.exports = new Battery()
