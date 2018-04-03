56fc395436046e0e22ee75a47607bf2b2b964ecd
lblblong <lblblong@qq.com>
2018-04-03 11:18:02 +0800

    删除无用的ctx赋值

--- middleware/format-output.js
+++ middleware/format-output.js
@@ -1,24 +1,24 @@
 module.exports = () => {
     return async (ctx, next) => {
         try {
-            let gtx = ctx
             await next()
-            if (ctx.type != "image/png") {
+            if (ctx.type != 'image/png') {
                 let body = ctx.body || {}
                 ctx.body = {
                     code: 0,
-                    message: "success.",
+                    message: 'success.',
                     data: body
                 }
             }
         } catch (err) {
             ctx.status = err.status || 200
-            let message = err.message || "error."
+            let message = err.message || 'error.'
             ctx.body = {
                 code: -1,
                 message,
                 data: {}
             }
+            let body = ctx.body
         }
     }
 }
