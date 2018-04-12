3ac454a45d1555943e101ca180dcb1bde788a786
lblblong <lblblong@qq.com>
2018-04-12 11:38:23 +0800

    添加koaBody解析表单数据到POST请求

--- middleware/router.js
+++ middleware/router.js
@@ -1,20 +1,21 @@
-const fs = require("fs")
+const fs = require('fs')
+var koaBody = require('koa-body')
 
 function addMapping(router, mapping) {
     for (var url in mapping) {
-        if (url.startsWith("GET ")) {
+        if (url.startsWith('GET ')) {
             var path = url.substring(4)
             router.get(path, mapping[url])
             console.log(`register URL mapping: GET ${path}`)
-        } else if (url.startsWith("POST ")) {
+        } else if (url.startsWith('POST ')) {
             var path = url.substring(5)
-            router.post(path, mapping[url])
+            router.post(path, koaBody({ multipart: true }), mapping[url])
             console.log(`register URL mapping: POST ${path}`)
-        } else if (url.startsWith("PUT ")) {
+        } else if (url.startsWith('PUT ')) {
             var path = url.substring(4)
             router.put(path, mapping[url])
             console.log(`register URL mapping: PUT ${path}`)
-        } else if (url.startsWith("DELETE ")) {
+        } else if (url.startsWith('DELETE ')) {
             var path = url.substring(7)
             router.del(path, mapping[url])
             console.log(`register URL mapping: DELETE ${path}`)
@@ -24,19 +25,19 @@ function addMapping(router, mapping) {
 
 function addApis(router, dir) {
     fs
-        .readdirSync(__dirname + "/" + dir)
+        .readdirSync(__dirname + '/' + dir)
         .filter(f => {
-            return f.endsWith(".js")
+            return f.endsWith('.js')
         })
         .forEach(f => {
-            let mapping = require(__dirname + "/" + dir + "/" + f)
+            let mapping = require(__dirname + '/' + dir + '/' + f)
             addMapping(router, mapping)
         })
 }
 
 module.exports = function(dir) {
-    let apisDir = dir || "apis",
-        Router = require("koa-router")
+    let apisDir = dir || 'apis',
+        Router = require('koa-router')
     let router = new Router()
     addApis(router, apisDir)
     return router
