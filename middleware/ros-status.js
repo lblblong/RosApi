bc572aa11d5686700ea2dcc64944511fb6ad66eb
lblblong <lblblong@qq.com>
2018-04-18 10:25:25 +0800

    与ros建立连接时异常状态为特殊值

--- middleware/ros-status.js
+++ middleware/ros-status.js
@@ -2,7 +2,9 @@ module.exports = () => {
     return async (ctx, next) => {
         let ros = global.ros
         if (!ros) {
-            throw Error("接口服务正在与 ROS 建立连接，此时接口不可操作")
+            let err = Error('接口服务正在与 ROS 建立连接，此时接口不可操作')
+            err.status = 404
+            throw err
         }
         await next()
     }
