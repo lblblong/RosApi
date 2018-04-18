fa118f220434f2ab97d6e6af6c9e079a436ad757
lblblong <lblblong@qq.com>
2018-04-18 10:31:45 +0800

    更换地图绘制算法

--- ros/Map.js
+++ ros/Map.js
@@ -3,69 +3,67 @@ var fs = require('fs')
 let PNGImage = require('pngjs-image')
 
 class Map {
-  constructor() {
-    this.data = ''
+    constructor() {
+        this.data = ''
 
-    global.event.on(global.events.ROS_CONNECTED, () => {
-      this.initDate()
-    })
+        global.event.on(global.events.ROS_CONNECTED, () => {
+            this.initDate()
+        })
 
-    global.event.on(global.events.ROS_DISCONNECTED, () => {
-      this.data = ''
-    })
-  }
+        global.event.on(global.events.ROS_DISCONNECTED, () => {
+            this.data = ''
+        })
+    }
 
-  initDate() {
-    let topic = new ROSLIB.Topic({
-      ros: global.ros,
-      name: '/map',
-      messageType: 'nav_msgs/OccupancyGrid'
-    })
+    initDate() {
+        let topic = new ROSLIB.Topic({
+            ros: global.ros,
+            name: '/map',
+            messageType: 'nav_msgs/OccupancyGrid'
+        })
 
-    // 奇怪的 bug ：
-    // 如果不延迟地图的订阅，会收不到位置信息
-    setTimeout(() => {
-      topic.subscribe(rep => {
-        console.log(`收到地图`)
-        this.data = rep
-        this.drawPng(rep)
-      })
-    }, 900)
-  }
+        // 奇怪的 bug ：
+        // 如果不延迟地图的订阅，会收不到位置信息
+        setTimeout(() => {
+            topic.subscribe(rep => {
+                console.log(`收到地图`)
+                this.data = rep
+                this.drawPng(rep)
+            })
+        }, 900)
+    }
 
-  drawPng(rep) {
-    let info = rep.info
-    let mapData = rep.data
-    let width = info.width
-    let height = info.height
-    var image = PNGImage.createImage(width, height)
+    drawPng(rep) {
+        let info = rep.info
+        let mapData = rep.data
+        let width = info.width
+        let height = info.height
+        var image = PNGImage.createImage(width, height)
 
-    for (let row = 0; row < height; row++) {
-      for (let col = 0; col < width; col++) {
-        let mapI = col + (height - row - 1) * width
-        let data = mapData[mapI]
-        let val
-        if (data === 100) {
-          val = 0
-        } else if (data === 0) {
-          val = 255
-        } else {
-          val = 127
+        let x, y, d
+        for (let i = 0; i < mapData.length; i++) {
+            x = parseInt(i % width)
+            y = parseInt(i / width)
+            d = parseInt(mapData[i])
+            let pix = 127
+            if (d == 100) {
+                pix = 0
+            } else if (d == 0) {
+                pix = 255
+            }
+            image.setPixel(x, y, {
+                red: pix,
+                green: pix,
+                blue: pix,
+                alpha: 255
+            })
         }
 
-        image.setAt(col, row, {
-          red: val,
-          green: val,
-          blue: val,
-          alpha: 255
+        image.writeImage('./map.png', function(err) {
+            if (err) console.log(err)
+            console.log(`地图已保存为图片`)
         })
-      }
     }
-    image.writeImage('./map.png', function(err) {
-      if (err) console.log(err)
-      console.log(`地图已保存为图片`)
-    })
-  }
 }
 
 module.exports = new Map()
