db335600df2daf4d55848549e34e91701865500b
lblblong <lblblong@qq.com>
2018-04-20 16:05:06 +0800

    添加异常捕获

--- apis/system.js
+++ apis/system.js
@@ -2,10 +2,52 @@ var call_sh = require('child_process')
 
 module.exports = {
     'GET /v1/system/mapping': async ctx => {
-        callsh('/home/ubuntu/zhrobot-ChangeService.sh', ['mapping'])
+        // callsh('/home/ubuntu/zhrobot-ChangeService.sh', ['mapping'])
+        try {
+            let rep = await callshSync(
+                '/home/ubuntu/zhrobot-ChangeService.sh',
+                ['mapping']
+            )
+            ctx.body = rep
+        } catch (e) {
+            throw Error('切换到建图模式失败')
+        }
     },
     'GET /v1/system/navigation': async ctx => {
-        callsh('/home/ubuntu/zhrobot-ChangeService.sh', ['navigation'])
+        // callsh('/home/ubuntu/zhrobot-ChangeService.sh', ['navigation'])
+        try {
+            let rep = await callshSync(
+                '/home/ubuntu/zhrobot-ChangeService.sh',
+                ['navigation']
+            )
+            ctx.body = rep
+        } catch (e) {
+            throw Error('切换到导航模式失败')
+        }
+    },
+    'GET /v1/system/test1': async ctx => {
+        // callsh('/home/ubuntu/zhrobot-ChangeService.sh', ['navigation'])
+        try {
+            let rep = await callshSync(
+                '/home/ubuntu/zhrobot-ChangeService.sh',
+                ['']
+            )
+            ctx.body = rep
+        } catch (e) {
+            throw Error('test1 error')
+        }
+    },
+    'GET /v1/system/test2': async ctx => {
+        // callsh('/home/ubuntu/zhrobot-ChangeService.sh', ['navigation'])
+        try {
+            let rep = await callshSync(
+                '/home/ubuntu/zhrobot-ChangeService.sh',
+                []
+            )
+            ctx.body = rep
+        } catch (e) {
+            throw Error('test2 error')
+        }
     }
 }
 
@@ -17,4 +59,14 @@ function callsh(file, para) {
             console.log('exec error: ' + error)
         }
     })
+}
+
+async function callshSync(file, para) {
+    try {
+        let rep = await call_sh.execFileSync(file, [...para])
+        rep = JSON.parse(rep)
+        return rep
+    } catch (e) {
+        throw e
+    }
 }
