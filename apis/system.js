f76afe3569ccca7577dd07bf6332f8e6b0f64de4
lbl <1162275271@qq.com>
2018-04-25 11:30:09 +0800

    修复匹配json串

--- apis/system.js
+++ apis/system.js
@@ -36,9 +36,12 @@ function callsh(file, para) {
 }
 
 async function callshSync(file, para) {
+    // 匹配第一个以 “{” 开头，“}” 结尾的字符串
+    let re = /\{[^\}]+\}/
     try {
         let rep = await call_sh.execFileSync(file, [...para])
         rep = rep.toString()
+        rep = rep.match(re)[0]
         rep = JSON.parse(rep)
         return rep
     } catch (e) {
