0a178ddc6b443e45f8759f47b97573ca295f8d78
lbl <1162275271@qq.com>
2018-04-27 15:45:14 +0800

    调用脚本代码封装

--- apis/system.js
+++ apis/system.js
@@ -1,4 +1,4 @@
-var call_sh = require('child_process')
+let { callshSync } = require('../util/call_sh')
 let logger = require('../log')
 
 module.exports = {
@@ -23,30 +23,5 @@ module.exports = {
         } catch (e) {
             throw Error('切换到导航模式失败')
         }
-    }
-}
-
-function callsh(file, para) {
-    call_sh.execFile(file, [...para], function(error, stdout, stderr) {
-        console.log('stdout: ' + stdout)
-        console.log('stderr: ' + stderr)
-        if (error !== null) {
-            console.log('exec error: ' + error)
-        }
-    })
-}
-
-async function callshSync(file, para) {
-    // 匹配第一个以 “{” 开头，“}” 结尾的字符串
-    let re = /\{[^\}]+\}/
-    try {
-        let rep = await call_sh.execFileSync(file, [...para])
-        rep = rep.toString()
-        rep = rep.match(re)[0]
-        rep = JSON.parse(rep)
-        return rep
-    } catch (e) {
-        logger.error(e.message)
-        throw e
     }
 }
