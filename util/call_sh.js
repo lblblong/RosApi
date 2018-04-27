61de24ef65f0ff659ee3584fc0990bf37f101521
lbl <1162275271@qq.com>
2018-04-27 15:45:56 +0800

    脚本封装

---
+++ util/call_sh.js
@@ -0,0 +1,20 @@
+var call_sh = require('child_process')
+
+async function callshSync(file, para) {
+    // 匹配第一个以 “{” 开头，“}” 结尾的字符串
+    let re = /\{[^\}]+\}/
+    try {
+        let rep = await call_sh.execFileSync(file, [...para])
+        rep = rep.toString()
+        rep = rep.match(re)[0]
+        rep = JSON.parse(rep)
+        return rep
+    } catch (e) {
+        logger.error(e.message)
+        throw e
+    }
+}
+
+module.exports = {
+    callshSync
+}
