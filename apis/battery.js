dff2f78fea747f24296dfcf637dd7c88d82605a8
lbl <1162275271@qq.com>
2018-04-27 21:03:21 +0800

    电池修改

--- apis/battery.js
+++ apis/battery.js
@@ -2,6 +2,9 @@ let battery = require('../ros/Battery')
 
 module.exports = {
     'GET /v1/battery': async ctx => {
+        if( battery.data){
+
+        }
         ctx.body = battery.data
     }
 }
